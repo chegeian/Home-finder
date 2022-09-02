3e37c910bac8dad0981ce4bd5084fc2ce7b59b16
Chege <chegeian@email.com>
2022-09-02 14:39:44 +0300

    js edit

--- JS/home.js
+++ JS/home.js
@@ -14,12 +14,51 @@ function autowrite(){
 
 setInterval(autowrite,100);
 
-
-///login
-
-$('form#contactForm').submit(function (event) {
+ //sending message function
+ $('form#contactForm').submit(function (event) {
     event.preventDefault();
-    const email= $('#typeEmailX-2').val();
-    const password= $('#typePasswordX-2').val();
-     alert("Email must be entered in a proper and valid format including unique characters e.g johndoe27@gmail.com");
+    const name = $('#name').val();
+    const pass = $('#email').val();
+    const mess = $('#mess').val();
+    alert("Hello " + name + "....Thank you we have received your message and we will respond to you as soon as possible. Thank you for contacting us.");
 });
+
+$(document).ready(function(){
+  console.log("main.js");
+
+
+  $("#findHere").click(function(){
+      var status = $("[name='status']").val()
+      var rooms = $("[name='rooms']").val()
+      var type = $("[name='type']").val()
+      var location = $("[name='location']").val()
+
+      if(status == 0){
+          localStorage.removeItem("status");
+      }
+      else{
+          localStorage.setItem("status", status);
+      }
+      if(rooms == 0){
+          localStorage.removeItem("room");
+      }
+      else{
+          localStorage.setItem("room", rooms);
+      }
+      if(type == 0){
+          localStorage.removeItem("type");
+      }
+      else{
+          localStorage.setItem("type", type);
+      }
+      if(location == 0){
+          localStorage.removeItem("location");
+      }
+      else{
+          localStorage.setItem("location", location);
+      }
+
+     alert ("Hello there,we are working on getting you the house with the preference you keyed in we wil send you the results via email in a few minutes")
+  })
+})
+
