793862fc6ee1d1231907f775d3dbc7a48ebe2b00
Chege <chegeian@email.com>
2022-09-02 14:51:07 +0300

    edit

--- JS/home.js
+++ JS/home.js
@@ -14,15 +14,6 @@ function autowrite(){
 
 setInterval(autowrite,100);
 
- //sending message function
- $('form#contactForm').submit(function (event) {
-    event.preventDefault();
-    const name = $('#name').val();
-    const pass = $('#email').val();
-    const mess = $('#mess').val();
-    alert("Hello " + name + "....Thank you we have received your message and we will respond to you as soon as possible. Thank you for contacting us.");
-});
-
 $(document).ready(function(){
   console.log("main.js");
 
@@ -61,4 +52,17 @@ $(document).ready(function(){
      alert ("Hello there,we are working on getting you the house with the preference you keyed in we wil send you the results via email in a few minutes")
   })
 })
+
+
+
+
+
+ //sending message function
+ $('form#contactForm').submit(function (event) {
+    event.preventDefault();
+    const name = $('#name').val();
+    const pass = $('#email').val();
+    const mess = $('#mess').val();
+    alert("Hello " + name + "....Thank you we have received your message and we will respond to you as soon as possible. Thank you for contacting us.");
+});
 
